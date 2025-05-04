b5b24b1d5e2e9631e22e878b3ea09966611f728d
Jean Pereira <jean.pereira@medsenior.com.br>
2025-05-04 18:17:24 -0300

    Descomenta gpio

--- Atuadores/Atuadores.py
+++ Atuadores/Atuadores.py
@@ -1,3 +1,5 @@
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
 import Aritmetica.Aritmetica as aritmetica
 import Configuracao.Configuracao as configuracao
 import threading
@@ -7,12 +9,12 @@ class Atuadores:
     posicao = {"dec": 0, "ra": 0}
 
     def __init__(self):
-        # GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
 
-        # self.motorDec = RpiMotorLib.A4988Nema(
-        #     configuracao.DIR_PIN_DEC, configuracao.STEP_PIN_DEC, (False, False, False), "A4988")
-        # self.motorRa = RpiMotorLib.A4988Nema(
-        #     configuracao.DIR_PIN_RA, configuracao.STEP_PIN_RA, (False, False, False), "A4988")
+        self.motorDec = RpiMotorLib.A4988Nema(
+            configuracao.DIR_PIN_DEC, configuracao.STEP_PIN_DEC, (False, False, False), "A4988")
+        self.motorRa = RpiMotorLib.A4988Nema(
+            configuracao.DIR_PIN_RA, configuracao.STEP_PIN_RA, (False, False, False), "A4988")
         # Os 3 últimos valores são para MS1, MS2, MS3 - False assume controle físico no driver
         self._homing()
 
@@ -29,9 +31,9 @@ class Atuadores:
         print(decRestante, raRestante)
 
         t1 = threading.Thread(target=self._mover_motor,
-                              args=(self.motorDec, decRestante))
+                              args=(None, decRestante))
         t2 = threading.Thread(target=self._mover_motor,
-                              args=(self.motorRa, raRestante))
+                              args=(None, raRestante))
 
         t1.start()
         t2.start()
@@ -39,23 +41,23 @@ class Atuadores:
         t1.join()
         t2.join()
 
-        self.posicao = (dec, ra)
+        self.posicao = {"dec": dec, "ra": ra}
 
     def _mover_motor(self, motor, passos):
         sentido = passos > 0
 
-        # motor.motor_go(
-        #     clockwise=sentido,
-        #     steptype="Full",
-        #     steps=passos,
-        #     stepdelay=0.01,
-        #     verbose=False,
-        #     initdelay=0.5
-        # )
+        motor.motor_go(
+            clockwise=sentido,
+            steptype="Full",
+            steps=passos,
+            stepdelay=0.01,
+            verbose=False,
+            initdelay=0.5
+        )
 
     def diferencaPosicaoParaAlvo(self, dec, ra):
-        dec = dec-self.posicao['dec']
-        ra = ra-self.posicao['ra']
+        dec = dec - self.posicao["dec"]
+        ra = ra - self.posicao["ra"]
         return dec, ra
 
     def __del__(self):
