41f5144fbb8ca1ba620d78941911e3ad4aaaf402
Jean Pereira <jean.pereira@medsenior.com.br>
2025-07-09 09:20:25 -0300

    testra

--- testra.py
+++ testra.py
@@ -1,5 +1,19 @@
-from domain.montagem import Montagem
+import RPi.GPIO as GPIO
+import time
+from RpiMotorLib import RpiMotorLib
+from RpiMotorLib.RpiMotorLib import A4988Nema
+import shared.configuracao as conf
 
-atuadores = Montagem()
 
-atuadores.apontar(-90, 0)
+motorDec = RpiMotorLib.A4988Nema(conf.DIR_PIN_DEC, conf.STEP_PIN_DEC, (True, True, True), "A4988")
+motorRa = RpiMotorLib.A4988Nema(conf.DIR_PIN_RA, conf.STEP_PIN_RA, (True, True, True), "A4988")
+
+while True:
+    motorRa.motor_go(
+            clockwise=True,
+            steptype=conf.TIPO_PASSO,
+            steps=18000,
+            stepdelay=0.001,
+            verbose=False,
+            initdelay=0.5
+        )
