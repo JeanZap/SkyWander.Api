fa2e0fdb687b014724a16ae61ba8733ef8c3ba3e
Jean Pereira <jean.pereira@medsenior.com.br>
2025-07-09 17:44:48 -0300

    testdec

--- testdec.py
+++ testdec.py
@@ -5,10 +5,10 @@ from RpiMotorLib.RpiMotorLib import A4988Nema
 import shared.configuracao as conf
 
 
-motorRa = RpiMotorLib.A4988Nema(conf.DIR_PIN_RA, conf.STEP_PIN_RA, (True, True, True), "A4988")
+motorDec = RpiMotorLib.A4988Nema(conf.DIR_PIN_DEC, conf.STEP_PIN_DEC, (True, True, True), "A4988")
 
 while True:
-    motorRa.motor_go(
+    motorDec.motor_go(
             clockwise=True,
             steptype=conf.TIPO_PASSO,
             steps=18000,
