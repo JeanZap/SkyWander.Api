096d177f25b693392d945dcb5e6f26d8506e9ba7
Jean Pereira <jean.pereira@medsenior.com.br>
2025-07-09 18:08:15 -0300

    testedec

--- testra.py
+++ testra.py
@@ -2,14 +2,15 @@ from RpiMotorLib import RpiMotorLib
 import shared.configuracao as conf
 
 
-motorRa = RpiMotorLib.A4988Nema(conf.DIR_PIN_RA, conf.STEP_PIN_RA, (True, True, True), "A4988")
+motorRa = RpiMotorLib.A4988Nema(
+    conf.DIR_PIN_RA, conf.STEP_PIN_RA, (True, True, True), "A4988"
+)
 
-while True:
-    motorRa.motor_go(
-            clockwise=True,
-            steptype=conf.TIPO_PASSO,
-            steps=18000,
-            stepdelay=0.001,
-            verbose=False,
-            initdelay=0.5
-        )
+motorRa.motor_go(
+    clockwise=True,
+    steptype=conf.TIPO_PASSO,
+    steps=18000,
+    stepdelay=0.001,
+    verbose=False,
+    initdelay=0.5,
+)
