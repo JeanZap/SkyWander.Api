cd745aa647b655c8e80881cd554f6c4559dcacd5
Jean Pereira <jean.pereira@medsenior.com.br>
2025-07-09 22:07:25 -0300

    de - 90 para 0 mudei o converter angulo de passos

--- domain/montagem.py
+++ domain/montagem.py
@@ -10,15 +10,15 @@ import time
 
 class Montagem:
     posicaoInicial = {
-        "dec": -90,
+        "dec": 0,
         "ra": 0,
-        "decPassos": aritmetica.converter_angulo_para_passos(-90),
+        "decPassos": aritmetica.converter_angulo_para_passos(0),
         "raPassos": aritmetica.converter_angulo_para_passos(0),
     }
     posicao = {
-        "dec": -90,
+        "dec": 0,
         "ra": 0,
-        "decPassos": aritmetica.converter_angulo_para_passos(-90),
+        "decPassos": aritmetica.converter_angulo_para_passos(0),
         "raPassos": aritmetica.converter_angulo_para_passos(0),
     }
     tracking_ativo = False
