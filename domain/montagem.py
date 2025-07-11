a36615de015a8c5d56db566121dcee487cec230d
Jean Pereira <jean.pereira@medsenior.com.br>
2025-07-11 18:44:43 -0300

    Proteção de angulos

--- domain/montagem.py
+++ domain/montagem.py
@@ -101,7 +101,7 @@ class Montagem:
         )
 
     def deve_proteger(self, ra: float):
-        return ra > 90 and ra <= 270
+        return ra > 0 and ra <= 180
 
     def converter_angulos_protegidos(self, dec: float, ra: float):
         if self.deve_proteger(ra):
