743d1f0fc6243383a005a0461aaf8a0431a6357c
Jean Pereira <jean.pereira@medsenior.com.br>
2025-12-06 17:07:11 -0300

    Acerta offset limit switch

--- debugg/testpin.py
+++ debugg/testpin.py
@@ -1,25 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-# Defina o número do pino que deseja usar
-PIN = 16  # você pode mudar para o pino desejado
-
-# Configura o modo de numeração e o pino
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.OUT)
 
-print(f"Pino {PIN} está em HIGH. Pressione Ctrl + C para encerrar.")
+PIN = 4  # GPIO que será lido
+
+# Se não tiver resistor externo, use pull-up ou pull-down
+GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:
-    # Mantém o pino em HIGH até o programa ser interrompido
     while True:
-        GPIO.output(PIN, GPIO.HIGH)
-        time.sleep(1)
+        estado = GPIO.input(PIN)
+        print("PIN HIGH" if estado else "PIN LOW")
+        time.sleep(0.2)  # intervalo de leitura
 
 except KeyboardInterrupt:
-    print("\nEncerrando script...")
+    print("\nEncerrando leitura...")
 
 finally:
-    # Limpa as configurações GPIO ao sair
     GPIO.cleanup()
-    print("GPIO restaurado ao estado padrão.")
