e93b3edfce91e61bc4fac55d8928192610f1a8fc
Jean Pereira <jean.pereira@medsenior.com.br>
2025-12-12 13:10:05 -0300

    Divide montagem em atuadores

--- domain/montagem.py
+++ domain/montagem.py
@@ -49,10 +49,10 @@ class Montagem:
             target=self.motor_ra.homing_motor,
         )
 
-        t1.start()
+        # t1.start()
         t2.start()
 
-        t1.join()
+        # t1.join()
         t2.join()
 
         print("Homing finalizado.")
